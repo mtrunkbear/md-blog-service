4706f37be2fd66b444c74aa8c76eff877f9ac862
mtrunkbear <migueltroncosogaldames@gmail.com>
2023-05-26 17:23:34 -0400

    REFACTOR removed junk file

--- src/setupProxy aaaaa.js
+++
@@ -1,5 +0,0 @@
-/* module.exports = function(app) {
-    app.use(proxy("/auth/google", { target: "http://localhost:4000" }));
-    app.use(proxy("/auth/google/*", { target: "http://localhost:4000" }));
-    app.use(proxy("/auth/google/callback/", { target: "http://localhost:4000" }));
-}; */
